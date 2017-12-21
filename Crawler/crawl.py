68c27827f23db2d191cd8afc85756042309bb194
Kamran Janjua <kamranejaz98@gmail.com>
2017-12-22 02:10:08 +0500

    Update crawl.py

--- Crawler/crawl.py
+++ Crawler/crawl.py
@@ -2,12 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
+
+#does basic crawling, gets the raw text.
+
 def getData(link):
 	req = requests.get(link)
 	soup = BeautifulSoup(req.content, 'html.parser')
-	parser_tag = soup.find_all('div', attrs={'class':'mw-parser-output'})
+	parser_tag = soup.find_all('div', attrs={'class':'mw-parser-output'}) #gets tags from class: mw-parser-output
 	for i in parser_tag:
-		print i.text
+		print i.text #prints the text from the tags returned from class: mw-parser-output
 
 if __name__ == '__main__':
 	keyword = "Nelson_Mandela"
