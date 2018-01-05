5731d8e8ab0a1abd27c1bcc38e056d27b4903d6e
Kamran Janjua <kamranejaz98@gmail.com>
2018-01-05 07:09:50 +0500

    Update crawl.py

--- Crawler/crawl.py
+++ Crawler/crawl.py
@@ -1,18 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
-import csv # to store the raw text, maybe xml or json in future?
+from collections import defaultdict
+import zlib
 
+class Parse:
+    count = 0
+    articleList = defaultdict(list)
 
-#does basic crawling, gets the raw text.
+    def __init__(self):
+        pass
 
-def getData(link):
-	req = requests.get(link)
-	soup = BeautifulSoup(req.content, 'html.parser')
-	parser_tag = soup.find_all('div', attrs={'class':'mw-parser-output'}) #gets tags from class: mw-parser-output
-	for i in parser_tag:
-		print i.text #prints the text from the tags returned from class: mw-parser-output
+    def fileNames(self):
+        self.outputFile = "ouput.dat"
+
+    def writeFile(self, text):
+        file = open(self.outputFile, 'w')
+        file.write("<parseRoot>") # first root.
+
+        file.write("</parseRoot>")
+        file.close()
+
+    def getData(self, url):  # does basic crawling, gets the raw text.
+        self.fileNames()
+        req = requests.get(url)
+        soup = BeautifulSoup(req.content, 'html.parser')
+        parser_tag = soup.find_all('div', attrs={'class': 'mw-parser-output'})  # gets tags from class: mw-parser-output
+        self.count += 1
+        for i in parser_tag:
+            self.articleList[self.count] = i.text.encode('utf-8')
+
+    def compressData(self): #compressing the text in the hashtable.
+        for key in self.articleList.iterkeys():
+            self.articleList[key] = zlib.compress(self.articleList[key])
 
 if __name__ == '__main__':
-	keyword = "Nelson_Mandela"
-	link = "https://simple.wikipedia.org/wiki/{}".format(keyword)
-	getData(link)
+    instance = Parse() #creating object of class.
+    keyword = ['Nelson Mandela', 'Cricket']
+    for i in keyword:
+        url = "https://simple.wikipedia.org/wiki/{}".format(i)
+        instance.getData(url)
+    instance.compressData()
+    print instance.articleList
