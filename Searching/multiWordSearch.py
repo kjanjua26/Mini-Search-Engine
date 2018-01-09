7c7c663514da7dacac8c45339f5c0da7378cb4db
Kamran Janjua <kamranejaz98@gmail.com>
2018-01-10 02:24:39 +0500

    Update multiWordSearch.py

--- Searching/multiWordSearch.py
+++ Searching/multiWordSearch.py
@@ -46,6 +46,7 @@ class Search:
         print "The words are in docs: ", ', '.join(self.combine(dList))
 
     def returnRes(self, query):
+        query = query.lower() # lower case the input since all keys are lower case!
         if ' ' in query: # faster than (len(query)>1) since len(query) for longer queries is time taking.
             self.doubleWord(query) 
         elif query.strip(): # checks if the string is not empty!
